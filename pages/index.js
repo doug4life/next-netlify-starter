4c01c3ffece90d2f906f36fcab8767cfa3039922
doug4life <70630864+doug4life@users.noreply.github.com>
2022-02-06 21:28:16 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Hi Maya GRRR <3" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
