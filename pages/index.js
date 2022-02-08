ab55a00a8c9d70a234430597f5ca893495bea893
doug4life <70630864+doug4life@users.noreply.github.com>
2022-02-07 21:44:52 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,11 +14,6 @@ export default function Home() {
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
-    <script>
-    function setup(){
-    background(100);
-    }
-     </script>
         </p>
       </main>
 
