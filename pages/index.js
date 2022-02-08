d4c94fd45eaaab43bb01719fcfb5a1e3ddea1185
doug4life <70630864+doug4life@users.noreply.github.com>
2022-02-07 23:09:03 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
- <!DOCTYPE html>
-<html>
-<head>
-<title>Title of the document</title>
-</head>
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
-<body>
-The content of the document......
-</body>
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+          <script src="p5.js"></script>
+          <script src="p5.sound.js"></script>
+          <script src="sketch.js"></script
+        <title>TESTIES!</title>
+        
+      </Head>
 
-</html> 
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
