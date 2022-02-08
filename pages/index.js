2fee494ece479bce90eed939f415b959ff778fa3
doug4life <70630864+doug4life@users.noreply.github.com>
2022-02-07 22:03:32 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,11 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+ <!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+</head>
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>GHOST OF JOSEF</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+<body>
+The content of the document......
+</body>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
+</html> 
